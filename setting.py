6d106c344fb0989e871b53faafdab9d3b16b8651
wolfsion <jison.c9@qq.com>
2022-02-17 19:30:09 +0800

    modify md

--- setting.py
+++ setting.py
@@ -1,9 +1,11 @@
 
 paper_tags = ('inproceedings', 'article')  ## only parse these tags
 sub_tags = ('booktitle', 'null')
+
 years = ('2016', '2017', '2018', '2019', '2020', '2021')
 url_text = ('/icml/', '/aaai/', '/nips/', '/ijcai/', '/iclr/',
             '/uai/', '/cvpr/', '/eccv/', '/tkde/', '/pami/')
+
 conf = ('ICML', 'AAAI', 'NeurIPS', 'IJCAI', 'ICLR', 'UAI', 'CVPR', 'ECCV')
 journal = ('TKDE', 'TPAMI')
 
