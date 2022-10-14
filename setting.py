a12347d3ce82bb422eb036be769c19d0f377593a
wolfsion <jison.c9@qq.com>
2022-10-14 19:30:22 +0800

    'sec'

--- setting.py
+++ setting.py
@@ -4,10 +4,11 @@ sub_tags = ('booktitle', 'null')
 
 years = ('2016', '2017', '2018', '2019', '2020', '2021')
 url_text = ('/icml/', '/aaai/', '/nips/', '/ijcai/', '/iclr/',
-            '/uai/', '/cvpr/', '/eccv/', '/tkde/', '/pami/')
+            '/uai/', '/cvpr/', '/eccv/', '/tkde/', '/pami/',
 
-conf = ('ICML', 'AAAI', 'NeurIPS', 'IJCAI', 'ICLR', 'UAI', 'CVPR', 'ECCV')
-journal = ('TKDE', 'TPAMI')
+            '/ndss/', '/ccs/', '/ieeesp/', '/uss/')
 
-# keywords = ['pruning', 'federated']
+# conf = ('ICML', 'AAAI', 'NeurIPS', 'IJCAI', 'ICLR', 'UAI', 'CVPR', 'ECCV')
+# journal = ('TKDE', 'TPAMI')
+
 keywords = ['federated']
